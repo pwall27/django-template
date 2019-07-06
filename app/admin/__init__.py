054fd80dcbfa440473746ea083ca9378f77630a1
Patrick Rodrigues <patrick.pwall@gmail.com>
2019-07-06 01:53:42 -0300

    Altera a estrutura inicial de App para suportar classes em diferentes arquivos

---
+++ app/admin/__init__.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .. import models
+
+# Import your custom admin classes here.
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
 from django.db import models
 
-# Create your models here.
+# Import your models here.
